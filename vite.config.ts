e20a7a6d4e583cc741bfb11e7d0a1653703ea8cf
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-08 16:25:50 +0100

    Setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    globals: true,
+    include: ['src/**/*.test.*'],
+    environment: 'jsdom',
+    setupFiles: 'src/test/setup-tests.ts',
+    coverage: {
+      // threshold
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+
+      // config
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['**/*.test.*', 'src/test', 'src/main.tsx', 'src/vite-env.d.ts'],
+      reporter: ['lcov', 'text-summary'],
+    },
+  },
+});
