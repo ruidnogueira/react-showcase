cf7f29447154567c0176256eb68e823567699aa0
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-08 10:42:00 +0100

    Add ls-lint

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import logo from './logo.svg';
-import styles from './App.module.scss';
+import styles from './app.module.scss';
 
 function App() {
   const [count, setCount] = useState(0);
