066be65d94ad17af984295aab2115a146acc07be
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-08 17:23:00 +0100

    chore: changes

--- src/app.tsx
+++ src/app.tsx
@@ -23,25 +23,6 @@ export function App() {
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className={styles.link}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   );
