d51e3c8d2470b845678783a3d6867521c19468aa
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-09 10:21:22 +0100

    Add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './build',
+  },
+
   plugins: [react()],
 
   // https://github.com/vitest-dev/vitest
