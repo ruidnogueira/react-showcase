2915f08c36e4c78976ac1460eaa7257c465a7c8f
ruidnogueira <81449309+ruidnogueira@users.noreply.github.com>
2022-06-10 18:58:48 +0100

    Add bundle analyzer (#18)
    build: add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vitePlugins } from './vite-plugins';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
     outDir: './build',
   },
 
-  plugins: [react(), ...vitePlugins],
+  plugins: [react(), visualizer({ filename: 'dependency-stats.html' }), ...vitePlugins],
 
   // https://github.com/vitest-dev/vitest
   test: {
