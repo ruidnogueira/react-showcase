b41cbbe41aa7bbbf06780d79a46ce8a537468ff3
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-11 15:03:56 +0100

    build: setup app deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import './index.scss';
+import urlJoin from 'url-join';
 
 if (import.meta.env.DEV && !import.meta.env.VITE_E2E) {
   const { worker } = await import('src/mocks/server/browser');
 
   await worker.start({
     onUnhandledRequest: 'bypass',
+    serviceWorker: {
+      url: urlJoin(import.meta.env.BASE_URL, 'mockServiceWorker.js'),
+    },
   });
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,54 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vitePlugins } from './vite-plugins';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: './build',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const baseUrl = env.VITE_BASE_URL;
 
-  plugins: [react(), visualizer({ filename: 'dependency-stats.html' }), ...vitePlugins],
+  return {
+    base: baseUrl,
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    globals: true,
-    include: ['src/**/*.test.*'],
-    environment: 'jsdom',
-    setupFiles: 'src/test/setup-tests.ts',
-    coverage: {
-      // threshold
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-
-      // config
-      all: true,
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: [
-        '**/*.test.*',
-        '**/*.stories.*',
-        'src/test',
-        'src/mocks',
-        'src/stories',
-        'src/main.tsx',
-        'src/vite-env.d.ts',
-      ],
-      reporter: ['lcov', 'text-summary'],
+    server: {
+      open: baseUrl,
     },
-  },
+
+    build: {
+      outDir: './build',
+    },
+
+    plugins: [react(), visualizer({ filename: 'dependency-stats.html' }), ...vitePlugins],
+
+    // https://github.com/vitest-dev/vitest
+    test: {
+      globals: true,
+      include: ['src/**/*.test.*'],
+      environment: 'jsdom',
+      setupFiles: 'src/test/setup-tests.ts',
+      coverage: {
+        // threshold
+        branches: 90,
+        functions: 90,
+        lines: 90,
+        statements: 90,
+
+        // config
+        all: true,
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: [
+          '**/*.test.*',
+          '**/*.stories.*',
+          'src/test',
+          'src/mocks',
+          'src/stories',
+          'src/main.tsx',
+          'src/vite-env.d.ts',
+        ],
+        reporter: ['lcov', 'text-summary'],
+      },
+    },
+  };
 });
