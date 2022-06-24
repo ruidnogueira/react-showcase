82eecc5babd8c63bda08657cdb51fbadeebee74a
Rui Nogueira <ruidnogueira@outlook.com>
2022-06-24 11:54:33 +0100

    fix: lint issues

--- src/app/utils/context.ts
+++ src/app/utils/context.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { createContext as createReactContext, useContext as useReactContext } from 'react';
 
 export interface CreateContextOptions {
