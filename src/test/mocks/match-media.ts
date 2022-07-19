388f0b23dbdf200487ab91febe7769e1b302bf89
ruidnogueira <81449309+ruidnogueira@users.noreply.github.com>
2022-07-19 20:28:28 +0100

    Improve global mocking (#98)
    chore: improve global mocking

--- src/test/mocks/match-media.ts
+++ src/test/mocks/match-media.ts
@@ -1,15 +1,14 @@
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: vi.fn().mockImplementation((query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(), // deprecated
-    removeListener: vi.fn(), // deprecated
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  })),
-});
+const matchMediaMock: typeof matchMedia = vi.fn().mockImplementation((query: string) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(), // deprecated
+  removeListener: vi.fn(), // deprecated
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}));
+
+vi.stubGlobal('matchMedia', matchMediaMock);
 
 export {};
