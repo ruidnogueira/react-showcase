904bd127b3a3aa9454f27ff6ecc9877901dad028
Rui Nogueira <ruidnogueira@outlook.com>
2022-10-23 23:04:51 +0100

    test: auth idle tests

--- src/app/modules/auth/auth-context.test.tsx
+++ src/app/modules/auth/auth-context.test.tsx
@@ -7,6 +7,7 @@ import { renderHook } from '@/test/helpers/render';
 import { act, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { AuthProvider, useAuth } from './auth-context';
+import userEvent from '@testing-library/user-event';
 
 function setup({ userId }: { userId: number }) {
   document.cookie = `userId=${userId}`;
@@ -35,88 +36,130 @@ afterEach(() => {
   vi.useRealTimers();
 });
 
-test('renders when provider exists', () => {
-  expect(() => setup({ userId: 1 })).not.toThrow();
+describe('context', () => {
+  test('renders when provider exists', () => {
+    expect(() => setup({ userId: 1 })).not.toThrow();
+  });
+
+  test('throws error if provider is missing', () => {
+    const logErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
+
+    expect(() => renderHook(() => useAuth())).toThrow();
+
+    logErrorSpy.mockRestore();
+  });
 });
 
-test('throws error if provider is missing', () => {
-  const logErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
+describe('check if logged in', () => {
+  test('user is logged out if it does not have a session', async () => {
+    const user = mockDatabase.user.create({ hasSession: false });
+    const { result } = setup({ userId: user.id });
 
-  expect(() => renderHook(() => useAuth())).toThrow();
+    expect(result.current.isCheckingIfLoggedIn).toBe(true);
+    expect(result.current.isLoggedIn).toBe(false);
 
-  logErrorSpy.mockRestore();
+    await waitFor(() => {
+      expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    });
+
+    expect(result.current.isLoggedIn).toBe(false);
+    expect(result.current.user).toBeNull();
+  });
+
+  test('user is logged in if it has a session', async () => {
+    const user = mockDatabase.user.create({ hasSession: true });
+    const { result } = setup({ userId: user.id });
+
+    expect(result.current.isCheckingIfLoggedIn).toBe(true);
+    expect(result.current.isLoggedIn).toBe(false);
+
+    await waitFor(() => {
+      expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    });
+
+    expect(result.current.isLoggedIn).toBe(true);
+    expect(result.current.user).toEqual(expect.objectContaining({ id: user.id }));
+  });
 });
 
-test('user is logged out if it does not have a session', async () => {
-  const user = mockDatabase.user.create({ hasSession: false });
-  const { result } = setup({ userId: user.id });
+describe('login', () => {
+  // TODO: error handling
 
-  expect(result.current.isCheckingIfLoggedIn).toBe(true);
-  expect(result.current.isLoggedIn).toBe(false);
+  test('user logs in', async () => {
+    const user = mockDatabase.user.create({ hasSession: false });
+    const { result } = setup({ userId: user.id });
 
-  await waitFor(() => {
-    expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    await waitFor(() => {
+      expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    });
+
+    act(() => {
+      void result.current.logIn({ email: user.email, password: user.password });
+    });
+
+    expect(result.current.isLoggingIn).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.isLoggingIn).toBe(false);
+    });
+
+    expect(result.current.isLoggedIn).toBe(true);
+    expect(result.current.user).toEqual(expect.objectContaining({ id: user.id }));
   });
-
-  expect(result.current.isLoggedIn).toBe(false);
-  expect(result.current.user).toBeNull();
 });
 
-test('user is logged in if it has a session', async () => {
-  const user = mockDatabase.user.create({ hasSession: true });
-  const { result } = setup({ userId: user.id });
+describe('logout', () => {
+  // TODO: error handling
 
-  expect(result.current.isCheckingIfLoggedIn).toBe(true);
-  expect(result.current.isLoggedIn).toBe(false);
+  test('user logs out', async () => {
+    const user = mockDatabase.user.create({ hasSession: true });
+    const { result } = setup({ userId: user.id });
 
-  await waitFor(() => {
-    expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    await waitFor(() => {
+      expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    });
+
+    act(() => {
+      void result.current.logOut();
+    });
+
+    expect(result.current.isLoggingOut).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.isLoggingOut).toBe(false);
+    });
+
+    expect(result.current.isLoggedIn).toBe(false);
+    expect(result.current.user).toBeNull();
   });
-
-  expect(result.current.isLoggedIn).toBe(true);
-  expect(result.current.user).toEqual(expect.objectContaining({ id: user.id }));
 });
 
-test('user logs in', async () => {
-  const user = mockDatabase.user.create({ hasSession: false });
-  const { result } = setup({ userId: user.id });
+describe('idle timer', () => {
+  test('user is logged out if idle for 15 min', async () => {
+    const user = mockDatabase.user.create({ hasSession: true });
+    const { result } = setup({ userId: user.id });
 
-  await waitFor(() => {
-    expect(result.current.isCheckingIfLoggedIn).toBe(false);
+    await waitFor(() => {
+      expect(result.current.isLoggedIn).toBe(true);
+    });
+
+    vi.advanceTimersByTime(15 * 60 * 1000);
+    await userEvent.click(document.body);
+
+    expect(result.current.isLoggedIn).toBe(false);
   });
 
-  act(() => {
-    void result.current.logIn({ email: user.email, password: user.password });
+  test('user is not logged out if idle for less than 15 min', async () => {
+    const user = mockDatabase.user.create({ hasSession: true });
+    const { result } = setup({ userId: user.id });
+
+    await waitFor(() => {
+      expect(result.current.isLoggedIn).toBe(true);
+    });
+
+    vi.advanceTimersByTime(14 * 60 * 1000);
+    await userEvent.click(document.body);
+
+    expect(result.current.isLoggedIn).toBe(true);
   });
-
-  expect(result.current.isLoggingIn).toBe(true);
-
-  await waitFor(() => {
-    expect(result.current.isLoggingIn).toBe(false);
-  });
-
-  expect(result.current.isLoggedIn).toBe(true);
-  expect(result.current.user).toEqual(expect.objectContaining({ id: user.id }));
-});
-
-test('user logs out', async () => {
-  const user = mockDatabase.user.create({ hasSession: true });
-  const { result } = setup({ userId: user.id });
-
-  await waitFor(() => {
-    expect(result.current.isCheckingIfLoggedIn).toBe(false);
-  });
-
-  act(() => {
-    void result.current.logOut();
-  });
-
-  expect(result.current.isLoggingOut).toBe(true);
-
-  await waitFor(() => {
-    expect(result.current.isLoggingOut).toBe(false);
-  });
-
-  expect(result.current.isLoggedIn).toBe(false);
-  expect(result.current.user).toBeNull();
 });
