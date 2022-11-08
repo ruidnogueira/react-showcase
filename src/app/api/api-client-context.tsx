a0a3b08f775504d880dc718390228cfc1f411321
Rui Nogueira <ruidnogueira@outlook.com>
2022-11-08 11:42:16 +0000

    feat: add api contexts tests

--- src/app/api/api-client-context.tsx
+++ src/app/api/api-client-context.tsx
@@ -4,19 +4,13 @@ import { ApiClient, createApi } from './api';
 
 const api = createApi();
 
-const [ApiClientContextProvider, useApiClient] = createContext<ApiClient>({
+const [ApiClientContextProvider, useApiClient, ApiClientContext] = createContext<ApiClient>({
   contextName: 'ApiClientContext',
   hookName: 'useApiClient',
 });
 
-export { useApiClient };
+export { useApiClient, ApiClientContext };
 
-export function ApiClientProvider({
-  api: providedApi,
-  children,
-}: {
-  children: ReactNode;
-  api?: ApiClient;
-}) {
-  return <ApiClientContextProvider value={providedApi ?? api}>{children}</ApiClientContextProvider>;
+export function ApiClientProvider({ children }: { children: ReactNode }) {
+  return <ApiClientContextProvider value={api}>{children}</ApiClientContextProvider>;
 }
